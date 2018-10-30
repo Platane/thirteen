bfaa3eef3ff8e19f75ff574e61ab812c413507c5
platane <me@platane.me>
2018-10-30 00:07:15 +0000

    bot, add some checks for the file structure

--- bot/src/analyzeSubmission/checks/file-structure.js
+++ bot/src/analyzeSubmission/checks/file-structure.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import kebabCase from 'lodash.kebabcase'
 import { readMainDirectory } from '../../readSubmission/readMainDirectory'
 
 export const key = 'file-structure'
@@ -8,17 +9,47 @@ export const description =
 export const check = ({ files, config }) => {
   const dir = readMainDirectory(files)
 
+  if (!files.every(({ status }) => status === 'added'))
+    return {
+      result: 'failure',
+      detail: [
+        'Every file should be new.',
+        "Please don't touch files that don't belong to you.",
+      ].join('\n'),
+    }
+
   if (!files.every(({ filename }) => path.normalize(filename).startsWith(dir)))
     return {
       result: 'failure',
       detail: 'All files are not into a submission folders',
     }
 
-  if (!dir.match(new RegExp(`^/?${config.submissionDir}[^/]+/?$`)))
+  const r = new RegExp(`^\/?${config.submissionDir}\/([^\/]+)\/?$`)
+  const [_, slug] = dir.match(r) || []
+
+  if (!slug)
     return {
       result: 'failure',
-      detail: 'Files are not inside a submission directory',
+      detail: [
+        'Files are not inside a submission directory',
+        `Files should be inside a directory named from your submission under the __${
+          config.submissionDir
+        }__ directory`,
+        '',
+        `For example : "${config.submissionDir}/my-awesome-game/"`,
+      ].join('\n'),
+    }
+
+  if (!isKebabCase(slug))
+    return {
+      result: 'failure',
+      detail: [
+        'Directory name should be kebab case',
+        `"${slug}" is not a valid directory name. Try "${kebabCase(slug)}" ?`,
+      ].join('\n'),
     }
 
   return true
 }
+
+const isKebabCase = x => x === kebabCase(x)
